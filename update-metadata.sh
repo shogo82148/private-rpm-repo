ea91104c86fea9e0df44180a6d46f99488c94dad
Ichinose Shogo <shogo82148@gmail.com>
2021-02-07 14:58:47 +0900

    ACLをつけ忘れた

--- update-metadata.sh
+++ update-metadata.sh
@@ -6,4 +6,4 @@ ROOT=$(cd "$(dirname "$0")" && pwd)
 
 cd "$ROOT"
 createrepo packages/amazonlinux/2/x86_64
-aws s3 sync packages/ s3://shogo82148-rpm-repository/
+aws s3 sync --acl=public-read packages/ s3://shogo82148-rpm-repository/
