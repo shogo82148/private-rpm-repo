b9fb71636bfceac895bf1ea353c5442eb9d9100f
Ichinose Shogo <shogo82148@gmail.com>
2021-02-16 18:50:51 +0900

    いらない子になりました

--- update-metadata.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -uex
-
-ROOT=$(cd "$(dirname "$0")" && pwd)
-
-cd "$ROOT"
-createrepo packages/amazonlinux/2/x86_64
-aws s3 sync --acl=public-read packages/ s3://shogo82148-rpm-repository/
