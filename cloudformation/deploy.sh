9ac991e386c69b399b095edefa765a99e62fce85
Ichinose Shogo <shogo82148@gmail.com>
2021-02-18 08:48:12 +0900

    add users for uploading rpm

--- cloudformation/deploy.sh
+++ cloudformation/deploy.sh
@@ -6,5 +6,13 @@ set -xue
 
 sam deploy \
     --region ap-northeast-1 \
-    --stack-name "rpm-repository" \
+    --stack-name "rpm-repository-ecr" \
+    --no-fail-on-empty-changeset \
     --template-file "${ROOT}/template.yaml"
+
+sam deploy \
+    --region ap-northeast-1 \
+    --stack-name "rpm-repository-users" \
+    --no-fail-on-empty-changeset \
+    --capabilities CAPABILITY_IAM \
+    --template-file "${ROOT}/users-template.yaml"
