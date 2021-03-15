b3f3976e3e328ee9c473efe70597b343aaf0c03b
Ichinose Shogo <shogo82148@gmail.com>
2021-03-15 10:58:47 +0900

    add upload script

---
+++ yum.repos/shogo82148/upload.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/env perl
+
+use utf8;
+use strict;
+use warnings;
+use FindBin;
+use File::Basename;
+
+sub execute {
+    my @arg = @_;
+    my $cmd = join " ", @arg;
+    print "executing: $cmd\n";
+    my $ret = system(@arg);
+    if ($ret != 0) {
+        print STDERR "::warning::failed to execute $cmd";
+    }
+}
+
+sub package_name {
+    my $file = shift;
+    my $name = basename $file;
+    $name =~ s/-[0-9]+\.[0-9]+\.[0-9]+-[0-9]+\..*$//;
+    return $name;
+}
+
+sub upload {
+    my ($variant, $prefix) = @_;
+    while (my $rpm = <$FindBin::Bin/$variant.build/RPMS/noarch/*.noarch.rpm>) {
+        my $package = package_name($rpm);
+        execute("aws", "s3", "cp", $rpm, "s3://shogo82148-rpm-temporary/$prefix/noarch/$package/");
+        execute("aws", "s3", "cp", $rpm, "s3://shogo82148-rpm-temporary/$prefix/x86_64/$package/");
+        execute("aws", "s3", "cp", $rpm, "s3://shogo82148-rpm-temporary/$prefix/aarch64/$package/");
+    }
+}
+
+upload "amazonlinux2", "amazonlinux/2";
+upload "centos7", "centos/7";
+upload "centos8", "centos/8";
