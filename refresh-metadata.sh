dd88670992706d82ed2ce801b829e65ccd13c079
Ichinose Shogo <shogo82148@gmail.com>
2022-11-29 15:35:37 +0900

    we need rpm only

--- refresh-metadata.sh
+++ refresh-metadata.sh
@@ -11,10 +11,10 @@ docker build -t "$IMAGE_NAME" .
 mkdir -p "$ROOT/.working"
 
 cd "$ROOT/.working"
-aws s3 sync --delete s3://shogo82148-rpm-repository .
+aws s3 sync --delete --exclude "*" --include "*.rpm" s3://shogo82148-rpm-repository .
 
 for distribution in */*/*; do
     docker run --rm -it -v "$ROOT/.working/$distribution:/repo" "$IMAGE_NAME" createrepo /repo
 done
 
-aws s3 sync . s3://shogo82148-rpm-repository
+aws s3 sync --acl public-read --exclude "*.rpm" . s3://shogo82148-rpm-repository
